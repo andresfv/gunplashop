8b279c7c394b4f47a18d0098d7c5dd77d50f5f99
andresfallasv <luis.fallas.valenciano@una.cr>
2022-10-20 18:53:46 -0600

    Se resolvió bug que impedia mostrar validaciones

--- src/pages/registration/RegistrationPage.jsx
+++ src/pages/registration/RegistrationPage.jsx
@@ -41,23 +41,23 @@ function RegistrationPage(params) {
 
                     <label htmlFor="lastname">LAST NAME*</label>
                     <input type="text" {...register("lastname", { required: true, maxLength: 100, minLength: 2 })} className="input" />
-                    {errors.name?.type === 'required' && "This field is required"}
-                    {(errors.name?.type === 'minLength' || errors.name?.type === 'maxLength') && <div className="error">Length of this field must be between 2 and 100 characters</div>}
+                    {errors.lastname?.type === 'required' && "This field is required"}
+                    {(errors.lastname?.type === 'minLength' || errors.name?.type === 'maxLength') && <div className="error">Length of this field must be between 2 and 100 characters</div>}
 
                     <label htmlFor="email">EMAIL*</label>
                     <input type="email" {...register("email", { required: true, maxLength: 50 })} className="input" />
-                    {errors.name?.type === 'required' && "This field is required"}
-                    {errors.name?.type === 'maxLength' && <div className="error">Max Length of this field must be less or equal to 50 characters</div>}
+                    {errors.email?.type === 'required' && "This field is required"}
+                    {errors.email?.type === 'maxLength' && <div className="error">Max Length of this field must be less or equal to 50 characters</div>}
 
                     <label htmlFor="username">USERNAME*</label>
                     <input type="text" {...register("username", { required: true, maxLength: 20 })} className="input" />
-                    {errors.name?.type === 'required' && "This field is required"}
-                    {errors.name?.type === 'maxLength' && <div className="error">Max Length of this field must be less or equal to 20 characters</div>}
+                    {errors.username?.type === 'required' && "This field is required"}
+                    {errors.username?.type === 'maxLength' && <div className="error">Max Length of this field must be less or equal to 20 characters</div>}
 
                     <label htmlFor="password">PASSWORD*</label>
                     <input type="password" {...register("password", { required: true, maxLength: 16, minLength: 8 })} className="input" />
-                    {errors.name?.type === 'required' && "This field is required"}
-                    {errors.name?.type === 'maxLength' && <div className="error">Length of this field must be between 8 and 16 characters</div>}
+                    {errors.password?.type === 'required' && "This field is required"}
+                    {errors.password?.type === 'maxLength' && <div className="error">Length of this field must be between 8 and 16 characters</div>}
                 </div>
                 <input type={"submit"} value="SEND" className="actionButtonReg" />
                 <Link to={"/login"}>
